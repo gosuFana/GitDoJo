1977549448f9a6f01be51b5ffeb4bda21fd75c70
dr. Simon Bálint <balladin0@gmail.com>
2019-10-01 10:44:06 +0200

    Modified introduce with new line

--- gittie.py
+++ gittie.py
@@ -1,8 +1,11 @@
 def introduce():
-    print("Hello, I am Gittie")
+    print("Hello, I am Gittie. \nI am docile.")
+    print("How are you today?")
+
 
 def add(a, b):
     print(2, 3)
+
 
 def joke():
     print("I would tell a joke if a knew one...")
